7ee1aba58155cdc5726512610db6bb6c68202d27
Cesar Carrasco <ccarrascocarre@gmail.com>
2026-04-08 11:04:34 -0400

    Se crea la clase de medico

---
+++ src/main/java/com/hospitalVM/atenciones/models/Medico.java
@@ -0,0 +1,40 @@
+package com.hospitalVM.atenciones.models;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+
+@Entity
+@Table(name = "medicos")
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class Medico {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "medico_id")
+    private Long medicoId;
+
+    @Column(unique = true, nullable = false)
+    @NotBlank(message = "El campo run no puede ser vacio")
+    @Pattern(regexp = "\\d(1,8)-[\\dKk]", message = "El formato del run debe ser xxxxxxxx-x")
+    private String run;
+
+    @Column(name = "nombre_completo", nullable = false)
+    @NotBlank(message = "El campo nombre completo no puede ser vacio")
+    private String nombreCompleto;
+
+    @Column(name="jefe_turno", nullable = false)
+    private Boolean jefeTurno;
+
+    @Embedded
+    private Audit audit =  new Audit();
+}
